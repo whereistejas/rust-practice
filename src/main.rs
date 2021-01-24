1796e137dfe5050584854404589d80230d7f778e
Tejas Sanap <sanap.tejas@gmail.com>
2021-01-24 23:44:57 +0530

    Generic types vol. I
    Signed-off-by: Tejas Sanap <sanap.tejas@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,26 @@
-extern crate argmin;
-use argmin::prelude::*;
-use argmin::solver::linesearch::HagerZhangLineSearch;
-
-struct cost_function {}
-
-// f(x)=(x−3) x^{3} (x−6)^{4}
-pub fn cf(x: f64) -> f64 {
-    let value: f64 = (x - 3 as f64) * x.powi(3) * (x - 6 as f64).powi(4);
-    value
-}
-
-// df(x) = (x - 6)**5 *
-// (x**3*(x - 6)*(x - 3)**(x**4)*(x + 4*(x - 3)*log(x - 3)) + 6*(x - 3)**(x**4 + 1)) /
-// (x - 3)
-pub fn cf_deriv(x: f64) -> f64 {
-    let value: f64 = (x - 6 as f64).powi(5)
-        * (x.powi(3)
-            * (x - 6 as f64)
-            * (x - 3 as f64).powf(x.powi(4))
-            * (x + 4 as f64 * (x - 3 as f64) * (x - 3 as f64).ln())
-            + 6 as f64 * (x - 3 as f64).powf(x.powi(4) + 1 as f64))
-        / (x - 3 as f64);
-    value
-}
-
-impl ArgminOp for cost_function {
-    type Param = f64;
-    type Output = f64;
-    type Float = f64;
-    type Jacobian = ();
-    type Hessian = ();
-
-    fn apply(&self, param: &Self::Param) -> Result<Self::Output, Error> {
-        Ok(cf(*param))
-    }
-
-    fn gradient(&self, param: &Self::Param) -> Result<Self::Param, Error> {
-        Ok(cf_deriv(*param))
-    }
-}
-
 fn main() {
-    let init_param: f64 = 0.0;
-    let operator = cost_function {};
+    let num_list = vec![1, 2, 3, 9, 8, 5];
 
-    let mut solver = HagerZhangLineSearch::new();
+    let largest = largest(&num_list);
 
-    solver.set_search_direction(vec![1.0]);
-    solver.set_init_alpha(0.5);
+    println!("largest: {}", largest);
+}
+
+// additions from The Rust Book
+// use of references
+//
+// peculiar observation: if we return a pointer from a function and
+// then assign it to a variable, it automatically takes the value to
+// which the reference is reffering.
+
+fn largest(num_list: &Vec<i64>) -> &i64 {
+    let mut largest = &num_list[0];
+
+    for num in num_list {
+        if num > largest {
+            largest = num;
+        }
+    }
+
+    largest
 }
