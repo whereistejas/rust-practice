7afba83d3fc496c6e3a781837365f47aa81ece5c
Tejas Sanap <sanap.tejas@gmail.com>
2021-02-01 20:15:39 +0530

    closures
    Signed-off-by: Tejas Sanap <sanap.tejas@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,31 @@
-use std::string::String;
+use rand::{thread_rng, Rng};
+use std::thread;
+use std::time::Duration;
 
-fn main() {
-    let str1 = String::from("Luke Skywalker");
-    let str2 = String::from("Anakin Skywalker");
-    
-    let result = longest_string(&str1, &str2);
-
-    println!("longest string: {}", result);
-
-    println!("string1: {}", str1);
+fn simulated_algorithm(intensity: u32) -> u32 {
+    println!("calculating slowly...");
+    thread::sleep(Duration::from_secs(2));
+    intensity
 }
 
-fn longest_string<'a> (str1: &'a String, str2: &'a String) -> &'a String {
-    if str1.len() > str2.len() {
-        str1
+fn generate_workout(intensity: u32, random: u32) {
+    if intensity < 25 {
+        println!("do pushups {} times", simulated_algorithm(intensity));
+        println!("do situps {} times", simulated_algorithm(intensity));
     } else {
-        str2
+        if random == 3 {
+            println!("rest for a day")
+        } else {
+        println!("run around for {} minutes", simulated_algorithm(intensity));
+        }
     }
+}
+
+fn main() {
+    let user_input = 50;
+
+    let mut generator = thread_rng();
+    let random_input = generator.gen_range(1..5);
+
+    generate_workout(user_input, random_input);
 }
