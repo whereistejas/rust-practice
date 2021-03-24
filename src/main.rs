d93f5e982fd80f61d0f41d0e8c09ae5c338ad3ed
Tejas Sanap <sanap.tejas@gmail.com>
2021-03-24 12:15:16 +0530

    Deref and drop traits for smart pointers
    also learned how to derive traits like `Debug`

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,51 @@
-enum List<T>{
-    Cons(T, Box<List<T>>),
-    Nil,
+use std::fmt::Debug;
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+struct CustomSmartPointer<T: Debug> {
+    data: T,
 }
 
-use crate::List::{Cons, Nil};
+impl<T> Drop for CustomSmartPointer<T>
+where
+    T: Debug,
+{
+    fn drop(&mut self) {
+        println!("dropping csp with data: {:?}", self.data);
+    }
+}
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
+}
 
 fn main() {
-    let list_int = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil) ) ) ) ) );
-    let list_char = Cons('a', Box::new(Cons('b', Box::new(Cons('c', Box::new(Nil))))));
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let name = MyBox::new(String::from("name"));
+    hello(&name);
+
+    let csp1 = CustomSmartPointer {
+        data: String::from("hello from hell"),
+    };
+    println!("{}", csp1.data);
 }
